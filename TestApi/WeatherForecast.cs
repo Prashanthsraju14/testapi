d4c8fb3d1e29488539d471dde914eb89778e87ac
prasanths2 <prasanths@astainnosys.com>
2025-06-30 15:44:16 +0530

    first commit

---
+++ TestApi/WeatherForecast.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace SecretManager
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public double TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
+namespace TestApi
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
