b24271b69340e05d8f96ca2efd0f3699f150eb66
prasanths2 <prasanths@astainnosys.com>
2025-07-01 12:23:18 +0530

    Revert "added sonarqube"
    This reverts commit 76cbc094ae40c52f4173f5f19996f58adc0432c8.

--- TestApi.Tests/UnitTest1.cs
+++ TestApi.Tests/UnitTest1.cs
@@ -1,26 +1,10 @@
-﻿using System.Net;
-using System.Threading.Tasks;
-using Xunit;
-using Microsoft.AspNetCore.Mvc.Testing;
+﻿namespace TestApi.Tests;
 
-namespace TestApi.Tests
+public class UnitTest1
 {
-    public class WeatherForecastControllerTests : IClassFixture<WebApplicationFactory<TestApi.Program>>
+    [Fact]
+    public void Test1()
     {
-        private readonly WebApplicationFactory<TestApi.Program> _factory;
 
-        public WeatherForecastControllerTests(WebApplicationFactory<TestApi.Program> factory)
-        {
-            _factory = factory;
-        }
-
-        [Fact]
-        public async Task Get_ReturnsSuccessAndJsonContentType()
-        {
-            var client = _factory.CreateClient();
-            var response = await client.GetAsync("/WeatherForecast");
-            response.EnsureSuccessStatusCode();
-            Assert.Equal("application/json; charset=utf-8", response.Content.Headers.ContentType.ToString());
-        }
     }
 }
