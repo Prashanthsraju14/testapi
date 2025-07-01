76cbc094ae40c52f4173f5f19996f58adc0432c8
prasanths2 <prasanths@astainnosys.com>
2025-07-01 12:19:48 +0530

    added sonarqube

--- TestApi.Tests/UnitTest1.cs
+++ TestApi.Tests/UnitTest1.cs
@@ -1,10 +1,26 @@
-﻿namespace TestApi.Tests;
+﻿using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+using Microsoft.AspNetCore.Mvc.Testing;
 
-public class UnitTest1
+namespace TestApi.Tests
 {
-    [Fact]
-    public void Test1()
+    public class WeatherForecastControllerTests : IClassFixture<WebApplicationFactory<TestApi.Program>>
     {
+        private readonly WebApplicationFactory<TestApi.Program> _factory;
 
+        public WeatherForecastControllerTests(WebApplicationFactory<TestApi.Program> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task Get_ReturnsSuccessAndJsonContentType()
+        {
+            var client = _factory.CreateClient();
+            var response = await client.GetAsync("/WeatherForecast");
+            response.EnsureSuccessStatusCode();
+            Assert.Equal("application/json; charset=utf-8", response.Content.Headers.ContentType.ToString());
+        }
     }
 }
