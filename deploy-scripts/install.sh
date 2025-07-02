966b1d7dd8666187e67bfe3811f15e6558896eb0
prasanths2 <prasanths@astainnosys.com>
2025-07-02 12:45:46 +0530

    fixed the systemd rename

--- deploy-scripts/install.sh
+++ deploy-scripts/install.sh
@@ -3,7 +3,7 @@ source /var/projectenv/config
 sudo rm -rf /var/www/$ENV/testapi
 sudo mkdir -p /var/www/$ENV/testapi
 sudo cp -a /var/tempbuild/code/testapi/out/. /var/www/$ENV/testapi/
-sudo cp -a /var/tempbuild/code/testapi/systemd/Xigify.testapi.$ENV.service /etc/systemd/system
+sudo cp -a /var/tempbuild/code/testapi/systemd/testapi.$ENV.service /etc/systemd/system
 sudo cp -a /var/www/$ENV/testapi/testapi.$ENV.appsettings.json /var/www/$ENV/testapi/testapi.appsettings.json 
 sudo systemctl daemon-reload
 sudo rm -rf /var/tempbuild/code/testapi
---
+++ readme.txt
@@ -0,0 +1,11 @@
+
+
+
+
+IAM roles:
+1.codepipline rolesploicies
+AmazonS3FullAccess
+AWSCodeDeployFullAccess
+AWSCodePipeline_FullAccess
+
+2.
